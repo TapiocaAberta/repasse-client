a8eca9f31ed6f5b822e549560a0967a829162646
Paulo Luan <pauloluan.inova@gmail.com>
2015-06-24 17:15:03 -0300

    fixed deploy task

--- gulp/deploy.js
+++ gulp/deploy.js
@@ -3,9 +3,7 @@
 var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
 
-module.exports = function(options) {
-    gulp.task('deploy', function() {
-        return gulp.src('./dist/**/*')
-            .pipe($.ghPages());
-    });
-};
+gulp.task('deploy', function() {
+    return gulp.src('./dist/**/*')
+        .pipe($.ghPages());
+});
